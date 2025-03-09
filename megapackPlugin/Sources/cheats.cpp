20b38a41e7644d43f9d50fbeb6c4507e807eba6d
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2025-03-09 16:18:03 -0400

    Add files via upload

---
+++ megapackPlugin/Sources/cheats.cpp
@@ -0,0 +1,60 @@
+#include "cheats.hpp"
+#include "3ds.h"
+
+namespace CTRPluginFramework
+{
+
+void dropEverything(MenuEntry *entry){
+    static int myInt = 1;
+    if (myInt == 1){
+        OSD::Notify("Use ZL+ZR to Drop Item in Hand.");
+        myInt++;
+    }
+    if (hidKeysDown() & (KEY_ZL | KEY_ZR))
+    {
+        Process::Write32(0xB329E8, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32948, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32A00, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32A18, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32A30, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32A48, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32A60, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32A78, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32A90, 0x02);
+        svcSleepThread(1000000);
+        Process::Write32(0xB32AB0, 0x02);
+        svcSleepThread(1000000);
+    }
+}
+
+void ninetyFov(MenuEntry *entry){
+    static int myInt = 1;
+    if (myInt == 1){
+        OSD::Notify("Set FOV to 90.");
+        myInt++;
+    }
+    float fovVal = 90;
+    Process::WriteFloat(0x3CEE80, fovVal);
+}
+
+void defaultCodes(MenuEntry *entry){
+    static int myInt = 1;
+    if (myInt == 1){
+        OSD::Notify("Set Megapack Default Codes.");
+        myInt++;
+    }
+    float swimVal = 0.075;
+    float viewBobVal = 10.0;
+    Process::WriteFloat(0x4EA090, swimVal);
+    Process::WriteFloat(0x3CF2A0, viewBobVal);
+}
+
+}
