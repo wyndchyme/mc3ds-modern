55e974c554ce5338b53079349ae8e2a21b4df25c
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2025-03-09 23:17:22 -0400

    update plg

--- megapackPlugin/Sources/cheats.cpp
+++ megapackPlugin/Sources/cheats.cpp
@@ -13,25 +13,45 @@ void dropEverything(MenuEntry *entry){
     if (hidKeysDown() & (KEY_ZL | KEY_ZR))
     {
         Process::Write32(0xB329E8, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB329E8, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32948, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32948, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32A00, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32A00, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32A18, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32A18, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32A30, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32A30, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32A48, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32A48, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32A60, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32A60, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32A78, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32A78, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32A90, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32A90, 0x00);
+        svcSleepThread(16666);
         Process::Write32(0xB32AB0, 0x02);
-        svcSleepThread(1000000);
+        svcSleepThread(16666);
+        Process::Write32(0xB32AB0, 0x00);
+        svcSleepThread(16666);
     }
 }
 
@@ -55,6 +75,17 @@ void defaultCodes(MenuEntry *entry){
     float viewBobVal = 10.0;
     Process::WriteFloat(0x4EA090, swimVal);
     Process::WriteFloat(0x3CF2A0, viewBobVal);
+}
+
+void removeMobCap(MenuEntry *entry){
+    Process::Write32(0xA33898, 0x00);
+    Process::Write32(0xA338A8, 0x00);
+    Process::Write32(0xA338AC, 0x00);
+    Process::Write32(0xA338B0, 0x00);
+    Process::Write32(0xA338B4, 0x00);
+    Process::Write32(0xA338B8, 0x00);
+    Process::Write32(0xA338BC, 0x00);
+    Process::Write32(0xA338C0, 0x00);
 }
 
 }
