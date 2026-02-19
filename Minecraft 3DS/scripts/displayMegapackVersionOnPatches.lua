661754ab4692e7cfbb3a8bd2234b673d465de735
wyndchyme <161674161+wyndchyme@users.noreply.github.com>
2026-02-19 13:57:43 -0600

    Updated Cracko's patch notes script

--- Minecraft 3DS/scripts/displayMegapackVersionOnPatches.lua
+++ Minecraft 3DS/scripts/displayMegapackVersionOnPatches.lua
@@ -2,10 +2,18 @@ local filesys = Core.Filesystem
 local debug = Core.Debug
 local memory = Core.Memory
 
+if not _G.cachedPatchString then
+    local patchFile = filesys.open(string.format('sdmc:/luma/titles/%s/romfs/notes/patch.txt', Core.getTitleId()), 'r')
+    if patchFile then
+        patchFile:seek(23)
+        _G.cachedPatchString = string.format("%s\0", patchFile:read(6))
+        patchFile:close()
+        memory.writeString(0x790478, _G.cachedPatchString, 6)
+        debug.log("File loaded and static address patched.")
+    end
+end
+
 local pointer = memory.readU32(0xA30CBC)
-local patchFile = filesys.open(string.format('sdmc:/luma/titles/%016X/romfs/notes/patch.txt', Core.getTitleId()))
-if patchFile then
-    patchFile:seek(23)
-    local fullString = string.format("%s\0", patchFile:read(5))
-    memory.writeString(pointer, fullString, 6)
-patchFile:close()
+if _G.cachedPatchString and pointer ~= 0 then
+    memory.writeString(pointer, _G.cachedPatchString, 6)
+end
