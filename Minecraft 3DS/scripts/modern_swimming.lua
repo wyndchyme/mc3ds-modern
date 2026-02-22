5ed0e18ed346e399c20e38cf91c957a1a0302290
Damienne <143151205+DamiansStupidCodeLMAO@users.noreply.github.com>
2026-02-22 01:33:43 -0330

    Move to Lunacore 0.15 and update swimming

--- Minecraft 3DS/scripts/modern_swimming.lua
+++ Minecraft 3DS/scripts/modern_swimming.lua
@@ -1,9 +1,25 @@
-Game.World.OnWorldJoin:Connect(function()
+function angToVec(pitch, yaw)
+	local yawRad = math.rad(yaw)
+	local pitchRad = math.rad(pitch)
+	local x = (-math.sin(yawRad) * math.cos(pitchRad))
+	local y = -math.sin(pitchRad)
+	local z = (math.cos(yawRad) * math.cos(pitchRad))
+	return {x=x, y=y, z=z}
+end
 
-    if Game.LocalPlayer.Sprinting == true then
-        Game.LocalPlayer.SwimSpeed = 0.06
-        Core.Debug.message("speed:"..Game.LocalPlayer.SwimSpeed)
-
-        else Game.LocalPlayer.SwimSpeed = 0.04
+Async.run(function ()
+    while Async.wait() do
+        if Game.World.Loaded then
+            if Game.LocalPlayer.Sprinting == true then
+		Game.LocalPlayer.SwimSpeed = 0.06
+		if Game.LocalPlayer.UnderWater == true then
+			local swimMovement = angToVec(Game.LocalPlayer.Camera.Pitch, Game.LocalPlayer.Camera.Yaw)
+			Game.LocalPlayer.Velocity.set(swimMovement.x*0.25, swimMovement.y*0.25, swimMovement.z*0.25)
+		end
+            else
+		Game.LocalPlayer.SwimSpeed = 0.04
+            end
+        end
     end
 end)
+
